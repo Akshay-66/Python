a5bee1c295523c72ba2830caee54e627b76fb468
Akshay-66 <akshay.mom9@gmail.com>
2023-04-11 23:32:47 +0530

    mom dont know that ive bought course,Sorry

---
+++ First.py
@@ -0,0 +1,48 @@
+
+
+
+
+# ##Upper case and Lower case
+# a = 'Hello'
+# b = "! This is Akshay"
+# c = a+b
+# # print(c.lower())
+# # print(c.upper())
+# d = c.split()
+# e = c.split('!') ## Delimiter
+# print(d[0])
+# print(d)
+# print(e)
+# ##Index characters
+# a = 'Champ'
+# print(a[3])
+# print(a[2:])
+# print(a[:2])
+# print(a[-1])
+# print(a[:-1])
+# print(a[::2])
+# print(a[::-1])
+
+
+# #Types of Output in Python
+# a = int(input("Enter First NUmber: "))
+# b = int(input("Enter Second Number: "))
+# sum = a+b
+# div = a/b
+# rem = a%b
+# print("Sum is: %d, Division is: %.2f, Remainder is: %d"%(sum,div,rem))
+# print("Sum is {0} Divided value is {1} Remainder of Division is {2}".format(sum,div,rem))
+# print(" {0:<3} | {1:^3} | {2:<3}".format("Total", "Quoient", "Remainder"))
+# print(" {0:<3} | {1:^3} | {2:<3}".format(sum, div, rem))
+
+# ##power
+# a = 8
+# b = 6
+# print(a**b)
+
+# ##Contatenation and length
+# a = "Gem"
+# b = ' python'
+# print(a+b)
+# print(len(a))
+
